b7b9fb3279ca32b2ed2f724b9a72b684bb48c354
houbass <laube.ondrej@seznam.cz>
2024-05-22 14:45:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+// ROUTER
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// PAGES
+import MainPage from "./pages/MainPage";
+
+
+// ROUTES
+const router = createBrowserRouter([
+  {
+      path: "/",
+      exact: true,
+      element: <MainPage />,
+  },
+]);
+
+
+
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
-export default App;
